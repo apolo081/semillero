2730c607e5ebaca64ee0cef6d6a212f03a98cd0f
minrock <lmoncarisg@gmail.com>
2014-01-17 21:22:40 -0500

    Eliminado db.py para que no lo rastree git

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from core.forms import CoordinadorForm
 from core.models import Coordinador,LiderGrupo,Estudiante
+
+class CoordinadorAdmin(admin.ModelAdmin):
+    add_form = CoordinadorForm
+    list_display = ('user.username','codigo','user.first_name','user.last_name')
 
 admin.site.register(Coordinador)
 admin.site.register(LiderGrupo)
