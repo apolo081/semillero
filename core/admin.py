e932356a485663fb42f7fd10a8f322ac40b5adfb
minrock <lmoncarisg@gmail.com>
2014-01-21 18:27:01 -0500

    Agregado change_form para los usuarios dentro de los perfiles.

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.contrib.admin import helpers
 from django.contrib.admin.options import ModelAdmin, IS_POPUP_VAR
+from django.contrib.admin.util import unquote
 from django.contrib.auth.models import User
 from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.forms.formsets import all_valid
 from django.forms.util import ErrorList
+from django.http.response import Http404
 from django.utils.encoding import force_text
 from core.models import Coordinador,LiderGrupo,Estudiante
 from django.utils.translation import ugettext as _
@@ -14,6 +17,7 @@ from semilleros.forms import UserForm
 
 class ProfileAdmin(ModelAdmin):
     add_form_template = '_add_user.html'
+    change_form_template = '_change_user.html'
     exclude = ['user',]
 
     def add_view(self, request, form_url='', extra_context=None):
@@ -111,6 +115,107 @@ class ProfileAdmin(ModelAdmin):
             }
         context.update(extra_context or {})
         return self.render_change_form(request, context, form_url=form_url, add=True)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        "The 'change' admin view for this model."
+        model = self.model
+        opts = model._meta
+
+        obj = self.get_object(request, unquote(object_id))
+
+        if not self.has_change_permission(request, obj):
+            raise PermissionDenied
+
+        if obj is None:
+            raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {'name': force_text(opts.verbose_name), 'key': escape(object_id)})
+
+        if request.method == 'POST' and "_saveasnew" in request.POST:
+            return self.add_view(request, form_url=reverse('admin:%s_%s_add' %
+                                    (opts.app_label, opts.model_name),
+                                    current_app=self.admin_site.name))
+
+        ModelForm = self.get_form(request, obj)
+        formsets = []
+        inline_instances = self.get_inline_instances(request, obj)
+        if request.method == 'POST':
+            form = ModelForm(request.POST, request.FILES, instance=obj)
+            if form.is_valid():
+                form_validated = True
+                new_object = self.save_form(request, form, change=True)
+            else:
+                form_validated = False
+                new_object = obj
+            prefixes = {}
+            for FormSet, inline in zip(self.get_formsets(request, new_object), inline_instances):
+                prefix = FormSet.get_default_prefix()
+                prefixes[prefix] = prefixes.get(prefix, 0) + 1
+                if prefixes[prefix] != 1 or not prefix:
+                    prefix = "%s-%s" % (prefix, prefixes[prefix])
+                formset = FormSet(request.POST, request.FILES,
+                                  instance=new_object, prefix=prefix,
+                                  queryset=inline.get_queryset(request))
+
+                formsets.append(formset)
+
+            if all_valid(formsets) and form_validated:
+                self.save_model(request, new_object, form, True)
+                self.save_related(request, form, formsets, True)
+                change_message = self.construct_change_message(request, form, formsets)
+                self.log_change(request, new_object, change_message)
+
+                if request.POST['first_name'] != new_object.user.first_name:
+                    new_object.user.first_name = request.POST['first_name']
+                if request.POST['last_name'] != new_object.user.last_name:
+                    new_object.user.last_name = request.POST['last_name']
+                if request.POST['email'] != new_object.user.email:
+                    new_object.user.email = request.POST['email']
+
+                new_object.user.save()
+                return self.response_change(request, new_object)
+
+        else:
+            form = ModelForm(instance=obj)
+            prefixes = {}
+            for FormSet, inline in zip(self.get_formsets(request, obj), inline_instances):
+                prefix = FormSet.get_default_prefix()
+                prefixes[prefix] = prefixes.get(prefix, 0) + 1
+                if prefixes[prefix] != 1 or not prefix:
+                    prefix = "%s-%s" % (prefix, prefixes[prefix])
+                formset = FormSet(instance=obj, prefix=prefix,
+                                  queryset=inline.get_queryset(request))
+                formsets.append(formset)
+
+        adminForm = helpers.AdminForm(form, self.get_fieldsets(request, obj),
+            self.get_prepopulated_fields(request, obj),
+            self.get_readonly_fields(request, obj),
+            model_admin=self)
+        media = self.media + adminForm.media
+
+        inline_admin_formsets = []
+        for inline, formset in zip(inline_instances, formsets):
+            fieldsets = list(inline.get_fieldsets(request, obj))
+            readonly = list(inline.get_readonly_fields(request, obj))
+            prepopulated = dict(inline.get_prepopulated_fields(request, obj))
+            inline_admin_formset = helpers.InlineAdminFormSet(inline, formset,
+                fieldsets, prepopulated, readonly, model_admin=self)
+            inline_admin_formsets.append(inline_admin_formset)
+            media = media + inline_admin_formset.media
+
+        context = {
+            'title': _('Change %s') % force_text(opts.verbose_name),
+            'adminform': adminForm,
+            'object_id': object_id,
+            'original': obj,
+            'is_popup': IS_POPUP_VAR in request.REQUEST,
+            'media': media,
+            'inline_admin_formsets': inline_admin_formsets,
+            'errors': helpers.AdminErrorList(form, formsets),
+            'app_label': opts.app_label,
+            'preserved_filters': self.get_preserved_filters(request),
+            'profile': obj.user,
+        }
+        context.update(extra_context or {})
+        return self.render_change_form(request, context, change=True, obj=obj, form_url=form_url)
 
 class CoordinadorAdmin(ProfileAdmin):
     pass
