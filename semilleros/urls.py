30569396da4bba58b5d9f8925de0954ba45671ce
minrock <lmoncarisg@gmail.com>
2014-01-21 18:50:13 -0500

    Configuradas las rutas de media y de archivos estaticos

--- semilleros/urls.py
+++ semilleros/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from semilleros import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +11,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+)
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
