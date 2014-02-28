8ac58012ef41a0fa2f513dd71e3150468e8a2beb
Jorge Ardila <jorge910907@gmail.com>
2014-02-27 22:31:04 -0500

    ahora si agregado home.html

--- core/views.py
+++ core/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, render_to_response, redirect
 from django.template.context import RequestContext
 
 def index(request):
-    return render_to_response('home/base.html',context_instance = RequestContext(request))
+    return render_to_response('home/home.html',context_instance = RequestContext(request))
